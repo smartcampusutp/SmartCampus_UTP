2ee7fa221e4369328aae9d9109fc06ab218e42c4
Balto <72516339+Balto16@users.noreply.github.com>
2025-09-15 11:12:40 -0500

    Start
    Subida inicial de código de Dashboard

---
+++ pages/Isla de Calor.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+from streamlit_autorefresh import st_autorefresh
+
+st.title("Dashboard - Isla de Calor")
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+# ESTILO CSV PARA QUE SE VEA BIEN BONITO
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# AUTOREFRESH CADA CIERTO TIEMPO
+st_autorefresh(interval=50000, limit=None, key="refresh_counter")
+
+# ------------------- CARGAR INFO -------------------
+df = pd.read_csv("Data/uplinks_2025-08-31.csv") # <-- NOMBRE DEL CSV REAL
+df['time'] = pd.to_datetime(df['time'], errors='coerce')
+
+# ------------------- BARRA DE SELECCIÓN -------------------
+st.sidebar.header('Dashboard - UTP')
+
+st.sidebar.header("Filtros")
+sensors = df["deviceName"].unique()
+selected_sensor = st.sidebar.selectbox("Seleccionar sensor", sensors)
+df_sensor = df[df["deviceName"] == selected_sensor]
+
+st.sidebar.subheader('Parámetros de visualización')
+plot_data = st.sidebar.multiselect(
+    'Seleccionar valores:',
+    ['temperature', 'humidity', 'pressure_hPa'],
+    ['temperature', 'humidity']
+)
+
+st.sidebar.markdown('''
+---
+Created by I2
+''')
+
+# ------------------- CUADRITOS KPI -------------------
+st.markdown('### Última Actualización')
+col1, col2, col3 = st.columns(3)
+
+latest = df.iloc[-1]  # ULTIMO VALOR
+col1.metric("Temperatura", f"{latest['temperature']:.2f} °C")
+col2.metric("Humedad", f"{latest['humidity']:.2f} %")
+col3.metric("Presión", f"{latest['pressure_hPa']:.0f} hPa")
+
+
+# ------------------- TABLA DE ÚLTIMOS 10 VALORES -------------------
+st.markdown("### Últimos 10 registros")
+
+df_table = df.drop(columns=["deviceName","battery_mV", "rssi","snr"])   # Eliminar columna
+st.dataframe(df_table.tail(10).iloc[::-1], use_container_width=True)    # Mostrar
+
+# -------------------- Grafico pritty que yo quiero ------------
+
+col1, col2 = st.columns(2)
+
+# ------------------ GAUGE TEMPERATURA ------------------
+with col1:
+    fig_temp = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=latest['temperature'],
+        title={'text': "Temperatura (°C)"},
+        gauge={
+            'axis': {'range': [0, 35]},
+            'bar': {'color': "red", 'thickness': 1},
+            'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 30}}
+            
+        
+    ))
+    st.plotly_chart(fig_temp, use_container_width=True)
+
+# ------------------ GAUGE HUMEDAD ------------------
+with col2:
+    fig_hum = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=latest['humidity'],
+        title={'text': "Humedad (%)"},
+        gauge={
+            'axis': {'range': [0, 100]},
+            'bar': {'color': "blue", 'thickness': 1},
+            'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 80}}
+        
+    ))
+    st.plotly_chart(fig_hum, use_container_width=True)
+
+
+# ------------------- GRAFICO TEMP HUMEDAD -------------------
+st.markdown('### Line chart')
+st.line_chart(df, x='time', y=plot_data, height=400)
