122e5e3ed1371f0c80eb5ce6d3131f5db34cd9ab
smartcampusutp <smartcampusutp@gmail.com>
2025-09-22 14:37:39 -0500

    Update Isla de Calor.py

--- pages/Isla de Calor.py
+++ pages/Isla de Calor.py
@@ -3,39 +3,91 @@ import pandas as pd
 import plotly.graph_objects as go
 from streamlit_autorefresh import st_autorefresh
 
-# ------------------- CONFIG -------------------
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.title("Dashboard - Isla de Calor")
 st.image("https://i.ibb.co/Q3RQT66R/SMT.png", caption=".")
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-# ------------------- ESTILO -------------------
+# ESTILO CSV PARA QUE SE VEA BIEN BONITO
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# ------------------- AUTOREFRESH -------------------
-st_autorefresh(interval=50000, limit=None, key="refresh_counter")  # cada 50 segundos
+# AUTOREFRESH CADA CIERTO TIEMPO
+st_autorefresh(interval=50000, limit=None, key="refresh_counter")
 
-# ------------------- CARGAR CSV -------------------
-df = pd.read_csv("Data/uplinks.csv")
+# ------------------- CARGAR INFO -------------------
+df = pd.read_csv("Data/uplinks.csv") # <-- NOMBRE DEL CSV REAL
 df['time'] = pd.to_datetime(df['time'], errors='coerce')
 
-# ------------------- FILTRAR SOLO NODOTEST -------------------
-df_sensor = df[df["deviceName"] == "NodoTest"]
+# ------------------- BARRA DE SELECCIÓN -------------------
+st.sidebar.header('Dashboard - UTP')
 
-# ------------------- SIDEBAR -------------------
-st.sidebar.header("Dashboard - UTP")
+st.sidebar.header("Filtros")
+sensors = df["deviceName"].unique()
+selected_sensor = st.sidebar.selectbox("Seleccionar sensor", sensors)
+df_sensor = df[df["deviceName"] == selected_sensor]
+
 st.sidebar.subheader('Parámetros de visualización')
 plot_data = st.sidebar.multiselect(
     'Seleccionar valores:',
     ['temperature', 'humidity', 'pressure_hPa'],
     ['temperature', 'humidity']
 )
-st.sidebar.markdown('''---\nCreated by I2''')
 
-# ------------------- KPIs -------------------
+st.sidebar.markdown('''
+---
+Created by I2
+''')
+
+# ------------------- CUADRITOS KPI -------------------
 st.markdown('### Última Actualización')
-latest = df_sensor.iloc[-1]  # último valor de NodoTest
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperatura", f"{latest['temperature']:.2f} °C")
-col2.metric("Humedad", f"{latest['humidity']:
 
+latest = df.iloc[-1]  # ULTIMO VALOR
+col1.metric("Temperatura", f"{latest['temperature']:.2f} °C")
+col2.metric("Humedad", f"{latest['humidity']:.2f} %")
+col3.metric("Presión", f"{latest['pressure_hPa']:.0f} hPa")
+
+
+# ------------------- TABLA DE ÚLTIMOS 10 VALORES -------------------
+st.markdown("### Últimos 10 registros")
+
+df_table = df.drop(columns=["deviceName","battery_mV", "rssi","snr"])   # Eliminar columna
+st.dataframe(df_table.tail(10).iloc[::-1], use_container_width=True)    # Mostrar
+
+# -------------------- Grafico pritty que yo quiero ------------
+
+col1, col2 = st.columns(2)
+
+# ------------------ GAUGE TEMPERATURA ------------------
+with col1:
+    fig_temp = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=latest['temperature'],
+        title={'text': "Temperatura (°C)"},
+        gauge={
+            'axis': {'range': [0, 35]},
+            'bar': {'color': "red", 'thickness': 1},
+            'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 30}}
+            
+        
+    ))
+    st.plotly_chart(fig_temp, use_container_width=True)
+
+# ------------------ GAUGE HUMEDAD ------------------
+with col2:
+    fig_hum = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=latest['humidity'],
+        title={'text': "Humedad (%)"},
+        gauge={
+            'axis': {'range': [0, 100]},
+            'bar': {'color': "blue", 'thickness': 1},
+            'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 80}}
+        
+    ))
+    st.plotly_chart(fig_hum, use_container_width=True)
+
+
+# ------------------- GRAFICO TEMP HUMEDAD -------------------
+st.markdown('### Line chart')
+st.line_chart(df, x='time', y=plot_data, height=400)
